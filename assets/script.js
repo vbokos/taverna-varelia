f7d7a4e4c798bdad29546fb1e37e725212a146f9
vbokos <bokos.vaggelis@gmail.com>
2022-06-21 01:22:21 +0300

    request to alter espa banner

--- assets/script.js
+++ assets/script.js
@@ -28,3 +28,11 @@ $('.carousel').swipe({
     },
     allowPageScroll: 'vertical'
 });
+
+$(document).ready(function() {
+    $('.template-article img').each(function() {
+        var currentImage = $(this);
+        currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
+    });
+    $('.image-link').magnificPopup({type:'image'});  
+  });
