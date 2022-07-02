2dc6563fe656d0cc909bcc730dc95327021b7817
vbokos <bokos.vaggelis@gmail.com>
2022-07-02 09:41:14 +0300

    fix error in magnific popup

--- assets/script.js
+++ assets/script.js
@@ -29,10 +29,3 @@ $('.carousel').swipe({
     allowPageScroll: 'vertical'
 });
 
-$(document).ready(function() {
-    $('.template-article img').each(function() {
-        var currentImage = $(this);
-        currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
-    });
-    $('.image-link').magnificPopup({type:'image'});  
-  });
