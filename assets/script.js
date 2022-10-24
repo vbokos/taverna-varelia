10f4a565feeea32785d52f326a26bc4c24dbbd8c
vbokos <bokos.vaggelis@gmail.com>
2022-10-24 20:53:29 +0300

    fix for disabled logo link

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    $(".scroll a, .navbar-brand, .gototop,.explore").click(function (event) {
+    $(".scroll a, .gototop,.explore").click(function (event) {
         event.preventDefault();
         $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 600, 'swing');
         $(".scroll li").removeClass('active');
